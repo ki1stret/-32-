73d03b532d4a20e1f8119a78c2fddeefe76807fc
ki1stret <114360739+ki1stret@users.noreply.github.com>
2023-06-26 10:05:31 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string partBookTitle = "C#.советы";
+            string insertText = "Народные ";
+            string bookTitle = partBookTitle.Insert(3, insertText);
+            MessageBox.Show(bookTitle);
+        }
+    }
+}
